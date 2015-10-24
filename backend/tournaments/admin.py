ce7fe58762d5e1087cf945deffa04db319dc26a3
Alexander Lyashuk <mooskagh@gmail.com>
2015-10-24 19:20:02 +0200

    Enable default admin UI for created model

--- backend/tournaments/admin.py
+++ backend/tournaments/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Member)
+admin.site.register(Tournament)
+admin.site.register(Round)
+admin.site.register(Bucket)
+admin.site.register(BucketPlayer)
+admin.site.register(TournamentPlayer)
+admin.site.register(Game)
+admin.site.register(GameForumMessage)
