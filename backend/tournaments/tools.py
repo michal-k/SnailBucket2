0a002bdd57c65f4364bdeca1d0cecd5e71985360
Alexander Lyashuk <mooskagh@gmail.com>
2016-02-02 22:45:42 +0100

    Separate http handlers from tournament logic.

---
+++ backend/tournaments/tools.py
@@ -0,0 +1,125 @@
+from . import models
+import datetime
+
+class NotFound(Exception):
+  """Exception which is thrown when something is not found."""
+
+
+class PermissionDenied(Exception):
+  """Exception thrown when someone doesn't have permission to do something."""
+
+
+def get_tournament(tournament):
+  """Gets tournament model instance.
+
+  Arguments:
+    tournament -- short name of tournament.
+
+  Return:
+    models.Tournament object instance.
+
+  Throws:
+    NotFound -- if tournament is not found.
+  """
+  try:
+    return models.Tournament.objects.get(short_name=tournament)
+  except models.Tournament.DoesNotExist:
+    raise NotFound("Tournament [%s] is not found." % tournament)
+
+
+def get_game(game_id):
+  """Gets game model instance.
+
+  Arguments:
+    game_id -- numeric id of a game.
+
+  Throws:
+    NotFound -- if game is not found.
+  """
+  try:
+    return models.Game.objects.get(id=game_id)
+  except models.Game.DoesNotExist:
+    raise NotFound("Game [%d] is not found." % game_id)
+
+
+def get_member(user):
+  """Returns member object from user.
+
+  Arguments:
+    user -- user model object.
+
+  Throws:
+    NotFound -- if user doesn't have corresponding user.
+  """
+  if not user:
+    raise PermissionDenied('No user specified.')
+  # Users who are not logged in are not tournament members.
+  if not user.is_authenticated():
+    raise PermissionDenied('User not authenticated')
+  try:
+    return models.Member.objects.get(user=user)
+  except models.Member.DoesNotExist:
+    raise NotFound("User [%s] does not have corresponding member."
+                   % user.username)
+
+
+def get_tournament_buckets(tournament):
+  """Return list of bucket of a given tournament, with list of participants per
+  bucket.
+
+  Arguments:
+    tournament -- short_name of a tournament.
+
+  Return:
+    List of dictionaries [{'name': <tournament name>,
+                           'members': [<username>, ...]}, ...]
+
+  Throws:
+    NotFound -- if tournament is not found.
+  """
+  buckets = get_tournament(tournament).bucket_set.all()
+  return [
+      { 'name': b.name,
+        'members': [ m.member.name() for m in b.players.all() ] }
+      for b in buckets ]
+
+
+def add_forum_message(game, member, text='',
+                      month=0, day=0, hour=0, minute=0, reset=False):
+"""Adds forum message.
+
+Arguments:
+  game -- game object.
+  member -- member object.
+  text -- text to add.
+  month, day, hour, minute -- if not zero, add that as game time.
+  reset -- reset game time.
+
+Return:
+  None
+
+Throws:
+  PermissionDenied -- if user doesn't have permission to add message.
+"""
+  # User must be one of players or one of TDs for that bucket.
+  if (member != game.white_player and
+      member != game.black_player and
+      member not in [t for t in game.bucket.tds.all()]):
+    raise PermissionDenied("User %s doesn't have permission to access game %d."
+                           % (member.user.username, game_id))
+  message = models.GameForumMessage(
+    game=game, member=member, text=text, time=datetime.datetime.now())
+  if reset:
+    message.reset_game_time = True
+  else:
+    year = datetime.date.today().year
+    # If months is in the past, then year is the next year.
+    if month < datetime.date.today().month:
+      year += 1
+    # If month and day are zero, don't set it.
+    if month > 0 and day > 0:
+      message.game_time = datetime.datetime(year, month, day, hour, minute)
+  message.save()
+
+
+  
--- backend/tournaments/views.py
+++ backend/tournaments/views.py
@@ -1,65 +1,41 @@
-from . import models
-from django.http import HttpResponse
+from . import tools
+from django.http import HttpResponse, Http404
 from django.shortcuts import get_object_or_404
 from django.core.exceptions import PermissionDenied
 import json
-import datetime
 
 def get_participants(request, tournament):
-  buckets = get_object_or_404(models.Tournament, short_name=tournament
-    ).bucket_set.all()
-  res = [ { 'name': b.name,
-            'members': [ m.member.name() for m in b.players.all() ] }
-          for b in buckets ]
+  try:
+    res = tools.get_tournament_buckets(tournament)
+  except tools.NotFound:
+    raise Http404
   return HttpResponse(json.dumps(res))
 
 
 def add_forum_message(request, game_id):
-  # Users who are not logged in cannot add forum messages.
-  if not request.user.is_authenticated():
-    raise PermissionDenied
-  game = get_object_or_404(models.Game, id=game_id)
-
-  # User must be one of players or one of TDs for that bucket.
-  if (request.user != game.white_player.user and
-      request.user != game.black_player.user and
-      request.user not in [t.user for t in game.bucket.tds.all()]):
-    raise PermissionDenied
-
-  # Convert user object to member object
   try:
-    member = models.Member.objects.get(user=request.user)
-  except modeks.Member.DoesNotExist:
-    return HttpResponse(status=500)
+    game = tools.get_game(game_id)
+    member = tools.get_member(request.user)
 
-  query = request.GET
-  # Forum message text is a required parameter.
-  try:
-    text = query['text']
-  except:
-    return HttpResponse(status=422)
-
-  message = models.GameForumMessage(
-    game=game, member=member, text=text, time=datetime.datetime.now())
-
-  if query.get('reset', '') == '1':
-    # This query has 'reset' flag.
-    message.reset_game_time = True
-  else:
+    query = request.GET
+    # Forum message text is a required parameter.
     try:
+      text = query['text']
+      reset = (query.get('reset', '') == '1')
       month = int(query.get('month', '0'))
       day = int(query.get('day', '0'))
       hour = int(query.get('hour', '0'))
       minute = int(query.get('minute', '0'))
-      year = datetime.date.today().year
-      if month < datetime.date.today().month:
-        year += 1
-      if month > 0 and day > 0:
-        message.game_time = datetime.datetime(year, month, day, hour, minute)
     except:
       return HttpResponse(status=422)
 
-  message.save()
+    tools.add_forum_message(game, member, month, day, hour, minute, reset)
+    # TODO(crem): Or what would be more appropriate response text?
+    return HttpResponse('OK!')
 
-  # TODO(crem): Or what would be more appropriate response text?
-  return HttpResponse('OK!')
+  except tools.NotFound:
+    raise Http404
+
+  except tools.PermissionDenied:
+    return HttpResponse(status=500)
+
