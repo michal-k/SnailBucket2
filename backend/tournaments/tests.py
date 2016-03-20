878c81a971d0f08e47880b9511770aef7e5fb36b
Alexander Lyashuk <mooskagh@gmail.com>
2016-03-20 11:33:55 +0100

    Create a library for registration.

--- backend/tournaments/tests.py
+++ backend/tournaments/tests.py
@@ -1,3 +1,18 @@
+from . import registration
+from .models import Member
 from django.test import TestCase
 
-# Create your tests here.
+class RegistrationTest(TestCase):
+  def test_successful_registration(self):
+    # Create token.
+    token = registration.GetRegistrationToken(
+      'GMKramnik', 'kramnik@kramnik.com')
+    # Check that token is valid.
+    decoded_token = registration.CheckRegistrationToken(token)
+    self.assertEqual(decoded_token,
+      {'username': 'GMKramnik', 'email': 'kramnik@kramnik.com'})
+    # Complete registration.
+    registration.CompleteRegistration(token, 'password')
+    # Check that user and member are created.
+    self.assertTrue(Member.objects.filter(user__username='GMKramnik').exists())
+
